2c57cd61621775d03807fe656e3415cd7509e009
Davidos <david.solobub@gmail.com>
2021-09-08 22:20:07 +0300

    upd

--- OOPLR2/OOPLR2/Form1.cs
+++ OOPLR2/OOPLR2/Form1.cs
@@ -25,12 +25,43 @@ namespace OOPLR2
         private void button2_Click(object sender, EventArgs e)
         {
             int count, fail_mail;
-            double weight;
+            double weight = 1;
             count = Convert.ToInt32(Interaction.InputBox("Введіть кількість листів", "Кількість листів", "4"));
             fail_mail = Convert.ToInt32(Interaction.InputBox("Введіть кількіcть не доставлених листів", "Кількість не доставлених листів", "1"));
             weight = Convert.ToDouble(Interaction.InputBox("Введіть вагу листів", "Вага листів", "0.2"));
             mail m2 = new mail(count, fail_mail, weight);
             m2.Info();
         }
+    }
+
+    class mail
+    {
+        int count, fail_mail;
+        double weight;
+
+        public mail(int count, int fail_mail, double weight)
+        {
+            this.count = count;
+            this.fail_mail = fail_mail;
+            this.weight = weight;
+        }
+
+        public double kg()
+        {
+            return count * weight;
+        }
+
+        public double delivered()
+        {
+            return count - fail_mail;
+        }
+
+        public void Info()
+        {
+            MessageBox.Show("Інформація про листи\n\nКількість листів: " + count.ToString() + "\nВага всіх листів: " +
+                kg().ToString() + "кг\nДоставлено листів: " + delivered().ToString() + "\nНе доставлено: " +
+                fail_mail.ToString(), "Пошта", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
     }
 }
--- OOPLR2/OOPLR2/Program.cs
+++ OOPLR2/OOPLR2/Program.cs
@@ -16,34 +16,4 @@ namespace OOPLR2
             Application.Run(new Form1());
         }
     }
-
-    class mail
-    {
-        int count, fail_mail;
-        double weight;
-
-        public mail()
-        {
-            int count = 20, fail_mail = 2;
-            double weight = 0.25;
-        }
-
-        public double kg()
-        {
-            return count * weight;
-        }
-
-        public double delivered()
-        {
-            return count - fail_mail;
-        }
-
-        public void Info()
-        {
-            MessageBox.Show("Інформація про листи\n\nКількість листів: " + count.ToString() + "\nВага всіх листів: " + 
-                kg().ToString() + "кг\nДоставлено листів: " + delivered().ToString() + "\nНе доставлено: " +
-                fail_mail.ToString(), "Пошта", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-
-    }
 }
